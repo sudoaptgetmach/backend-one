53bf3ef1d269bd5e9d6c7ea3d4524d03f5d66c1b
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-14 03:09:07 -0300

    Completando a primeira parte do curso :)

---
+++ src/main/java/org/mach/src/primeiraaplicacao/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package org.mach.src.primeiraaplicacao;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Conclui a aula 01 e agora estou mergulhando em Java!");
+    }
+
+}
---
+++ src/main/java/org/mach/src/teste.java
@@ -0,0 +1,4 @@
+package org.mach.src;
+
+public class teste {
+}
