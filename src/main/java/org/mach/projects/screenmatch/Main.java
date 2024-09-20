f0a3572d0c7a5f2e5064cc4db5c5946b1df39a8e
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-20 03:06:57 -0300

    Adicionando (e começando +/-) os arquivos do projeto Screenmatch

--- src/main/java/org/mach/projects/screenmatch/Main.java
+++ src/main/java/org/mach/projects/screenmatch/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
         int anoDeLancamento;
         anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
 
         System.out.println("Esse é o screenmatch");
         System.out.println("Filme: Top Gun Maverick");
-
-        System.out.println(anoDeLancamento);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
         System.out.println();
-      
+
     }
 
 }
