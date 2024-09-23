2a288a9a3a3fdd2f26b0ee9adc21773a5d7745ae
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 02:14:43 -0300

    Conclusão do exercício: média

--- src/main/java/org/mach/src/primeiraaplicacao/desafios/Media.java
+++ src/main/java/org/mach/src/primeiraaplicacao/desafios/Media.java
@@ -4,6 +4,13 @@ public class Media {
 
     public static void main(String[] args) {
 
+        double num1 = Math.random() * 11;
+        double num2 = Math.random() * 11;
+        float media = (float) (num1 + num2) / 3;
+
+        System.out.printf("Primeira nota: %.1f\n", num1);
+        System.out.printf("Segunda nota: %.1f\n", num2);
+        System.out.printf("Média: %.1f\n", media);
 
 
     }
