ba178abe19e2022772868e1b5b89190cd4de46c6
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 01:51:56 -0300

    Adicionando (e começando +/-) os arquivos do projeto Screenmatch

--- src/main/java/org/mach/projects/screenmatch/Main.java
+++ src/main/java/org/mach/projects/screenmatch/Main.java
@@ -8,11 +8,17 @@ public class Main {
         anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        String sinopse;
+        sinopse = """
+                  After more than 30 years of service as one of the Navy's top aviators, Pete "Maverick" Mitchell is where he belongs, 
+                  pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.
+                  """;
 
         System.out.println("Esse é o screenmatch");
         System.out.println("Filme: Top Gun Maverick");
         System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println();
+        System.out.println("Sinopse: \n" + sinopse);
+
 
     }
 
