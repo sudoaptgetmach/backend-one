bd970d646bcc33ced80e76e1c9fc7850bae43f62
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 02:24:52 -0300

    Fim do exercício de concatenação de Strings e Char

---
+++ src/main/java/org/mach/src/primeiraaplicacao/desafios/CharAndString.java
@@ -0,0 +1,15 @@
+package org.mach.src.primeiraaplicacao.desafios;
+
+public class CharAndString {
+
+    public static void main(String[] args) {
+
+        char[] letra = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+        String palavra;
+        palavra = "Vou te mostrar as 7 primeiras letras do alfabeto: ";
+
+        System.out.println(palavra);
+        System.out.println(letra[0] + " " + letra[1] + " " + letra[2] + " " + letra[3] + " " + letra[4] + " " + letra[5] + " " + letra[6]);
+
+    }
+}
