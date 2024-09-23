48491c04e83fb0cf545becede9d542c46cf36b4c
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 02:32:15 -0300

    Fim do exercício de multiplicação produto

--- src/main/java/org/mach/src/primeiraaplicacao/desafios/Produto.java
+++ src/main/java/org/mach/src/primeiraaplicacao/desafios/Produto.java
@@ -4,7 +4,13 @@ public class Produto {
 
     public static void main(String[] args) {
 
+        int quantidade = (int) (Math.random() * 101);
+        double precoProduto = Math.random() * 1001;
+        double resultado = quantidade * precoProduto;
 
+        System.out.println("Quantidade de produtos: " + quantidade);
+        System.out.printf("Valor dos produtos: %.2f\n", precoProduto);
+        System.out.printf("Preço total: %.2f\n", resultado);
 
     }
 }
