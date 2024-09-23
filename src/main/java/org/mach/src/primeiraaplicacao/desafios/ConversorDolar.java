867fca7a91fc978460b75f2da28b86c9dcbea772
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 02:38:13 -0300

    Conversor de Dólares para Reais adicionado. (Considere que o dólar vale 4.94)

---
+++ src/main/java/org/mach/src/primeiraaplicacao/desafios/ConversorDolar.java
@@ -0,0 +1,18 @@
+package org.mach.src.primeiraaplicacao.desafios;
+
+public class ConversorDolar {
+
+    private static final int QUANTIDADE_MAX = 100000;
+    private static final double VALOR_DOLAR = 4.94;
+
+
+    public static void main(String[] args) {
+
+        double valorEmDolares = Math.random() * QUANTIDADE_MAX;
+        double valorEmReais = valorEmDolares * VALOR_DOLAR;
+
+        System.out.printf("Valor em dólares: %.2f\n", valorEmDolares);
+        System.out.printf("Valor da conversão de Dólares para Reais: %.2f", valorEmReais);
+
+    }
+}
