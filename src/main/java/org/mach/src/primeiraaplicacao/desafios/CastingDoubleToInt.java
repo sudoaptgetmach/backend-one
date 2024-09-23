2ad6618034e8549741a0604887972c1f25005750
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-23 02:19:57 -0300

    Fim do exercício de casting

--- src/main/java/org/mach/src/primeiraaplicacao/desafios/CastingDoubleToInt.java
+++ src/main/java/org/mach/src/primeiraaplicacao/desafios/CastingDoubleToInt.java
@@ -4,6 +4,11 @@ public class CastingDoubleToInt {
 
     public static void main(String[] args) {
 
-    }
+        double variavel = Math.random() * 6;
+        int doubleToInt = (int) variavel;
 
+        System.out.println("Variável double: " + variavel);
+        System.out.println("Variável após casting: " + doubleToInt);
+
+    }
 }
