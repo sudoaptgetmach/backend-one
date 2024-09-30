8bb07d133c7f2ce566248114870f0e8227fdb4dc
sudoaptgetmach <qluizmachado@gmail.com>
2024-09-30 00:32:56 -0300

    Add initial project structure

---
+++ src/main/java/org/mach/src/exercises/beecrowd/extremelybasic.java
@@ -0,0 +1,21 @@
+package org.mach.src.exercises.beecrowd;
+
+import java.util.Scanner;
+
+public class extremelybasic {
+    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite dois números: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int resultado = a + b;
+
+
+        System.out.println("X = " + resultado);
+        
+
+    }
+}
---
+++ src/main/java/org/mach/src/exercises/beecrowd/helloworld.java
@@ -0,0 +1,8 @@
+package org.mach.src.exercises.beecrowd;
+
+public class helloworld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
