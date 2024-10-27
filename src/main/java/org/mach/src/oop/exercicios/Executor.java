af8361dec49692e0289d494043da885cbefaf5b1
Luiz <qluizmachado@gmail.com>
2024-10-26 23:48:50 -0300

    Added: ConversorMoeda, Executor, Pessoa.java
    Took 32 minutes

--- src/main/java/org/mach/src/oop/exercicios/Executor.java
+++ src/main/java/org/mach/src/oop/exercicios/Executor.java
@@ -1,22 +1,27 @@
 package org.mach.src.oop.exercicios;
 
-import org.mach.src.oop.exercicios.reaproveitandocaracteristicas.GeradorPrimo;
-import org.mach.src.oop.exercicios.reaproveitandocaracteristicas.VerificadorPrimo;
+import org.mach.src.oop.exercicios.getterssetters.ContaBancaria;
+import org.mach.src.oop.exercicios.getterssetters.Pessoa;
+import org.mach.src.oop.exercicios.reaproveitandocaracteristicas.Cachorro;
+
+import java.util.ArrayList;
 
 public class Executor {
 
     public static void main(String[] args) {
 
-        VerificadorPrimo verificador = new VerificadorPrimo();
-        GeradorPrimo gerador = new GeradorPrimo();
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
 
-        verificador.setPrimo(1);
+        Pessoa pessoa1 = new Pessoa("Pessoa1", (int) (Math.random() * 100));
+        Pessoa pessoa2 = new Pessoa("Pessoa2", (int) (Math.random() * 100));
+        Pessoa pessoa3 = new Pessoa("Pessoa3", (int) (Math.random() * 100));
 
-        gerador.gerarProximoPrimo();
-        gerador.gerarProximoPrimo();
-        gerador.gerarProximoPrimo();
+        listaDePessoas.add(pessoa1);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
 
-        boolean ehPrimo = verificador.verificarSeEhPrimo();
-        System.out.println("O número é primo? " + ((ehPrimo) ? "Sim." : "Não."));
+        System.out.println(listaDePessoas.get(0).toString());
+        System.out.println(listaDePessoas.size());
+        System.out.println(listaDePessoas.toString());
     }
 }
--- src/main/java/org/mach/src/oop/exercicios/getterssetters/Pessoa.java
+++ src/main/java/org/mach/src/oop/exercicios/getterssetters/Pessoa.java
@@ -1,8 +1,36 @@
 package org.mach.src.oop.exercicios.getterssetters;
 
+import java.util.ArrayList;
+
 public class Pessoa {
+
+    private final String nome;
+    private final int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
 
     public String getFrase() {
         return "Olá, mundo.";
+    }
+
+    public String toString() {
+        return "Nome: " + nome + "\nIdade: " + idade + "\n";
+    }
+
+    public void run() {
+
+
+
     }
 }
